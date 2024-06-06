e7c4e5df5000a6415f38c6697393a22ade43dec5
Raúl Ferràs <raul.ferras@lifullconnect.com>
2024-06-06 18:46:36 +0200

    Update treesitter to fix compilation. Add highlights.scm with some queries.

---
+++ queries/highlights.scm
@@ -0,0 +1,130 @@
+(global_declaration 
+	(ident) @variable)
+(type
+  (base_type (base_type_name) @type))
+
+(type_ident) @type
+
+[
+  "const"
+  "enum"
+  "extern"
+  "inline"
+  "struct"
+  "union"
+  "module"
+  "def"
+  "fault"
+  "defer"
+] @keyword
+
+"import" @include
+
+[
+  "return"
+] @keyword.return
+
+[
+  "fn"
+] @keyword.function
+
+[
+  "while"
+  "for"
+] @repeat
+
+[
+  "if"
+  "else"
+  "switch"
+] @conditional
+
+[
+  "="
+  "-"
+  "*"
+  "/"
+  "+"
+  "%"
+  
+  "~"
+  "|"
+  "&"
+  "&&"
+  "^"
+  "<<"
+  ">>"
+  
+  "<"
+  "<="
+  ">="
+  ">"
+  "=="
+  "!="
+  
+  "!"
+  "&&"
+  "||"
+  
+  "-="
+  "+="
+  "*="
+  "/="
+  "%="
+  "|="
+  "&="
+  "^="
+  ">>="
+  "<<="
+  "--"
+  "++"
+  
+  ".."
+
+  "=>"
+] @operator
+
+[
+  "."
+  ";"
+  ":"
+  "::"
+  ","
+] @punctuation.delimiter
+
+"..." @punctuation.special
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+["null"] @constant.builtin
+(string_literal) @string
+(escape_sequence) @string.escape
+(integer_literal) @number
+(real_literal) @number
+(char_literal) @character
+
+(field_expr argument: (ident) @property)
+(field_expr field: (access_ident) @type (#match? @type "^_?[A-Z]"))
+
+(module path: (path_ident) @namespace)
+(module (path_ident (ident) @namespace .))
+(import_declaration (path_ident) @namespace)
+(import_declaration (path_ident (ident) @namespace .))
+
+(module_ident_expr (module_resolution) @namespace)
+(module_ident_expr [(const_ident)(ident)(at_ident)] @identifier)
+
+(call_expr function: (ident) @function)
+
+[
+ (line_comment)
+ (doc_comment)
+ (block_comment)
+ ] @comment
