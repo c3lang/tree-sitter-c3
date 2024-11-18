4a08dd0ca915562531475d424d4dec300e59cf7f
Tomas Kallup <t.kallup@gmail.com>
2024-11-18 10:30:49 +0100

    Fix: def for aliasing a method
    `def foo = Foo.foo;` should now be highlighted properly

--- grammar.js
+++ grammar.js
@@ -203,9 +203,18 @@ module.exports = grammar({
     )),
     _module_path: $ => repeat1($.module_resolution),
     path_ident: $ => seq(optional($._module_path), $.ident),
+    define_path_ident: $ => seq(
+      optional($._module_path),
+      optional(seq($.type_ident,'.')),
+      $.ident,
+    ),
+    define_path_at_ident: $ => seq(
+      optional($._module_path),
+      optional(seq($.type_ident,'.')),
+      $.at_ident
+    ),
     path_type_ident: $ => seq(optional($._module_path), $.type_ident),
     path_const_ident: $ => seq(optional($._module_path), $.const_ident),
-    path_at_ident: $ => seq(optional($._module_path), $.at_ident),
     path_at_type_ident: $ => seq(optional($._module_path), $.at_type_ident),
 
     // Generic Parameters
@@ -354,9 +363,9 @@ module.exports = grammar({
     ),
     define_ident: $ => seq(
       choice(
-        seq($.ident, '=', $.path_ident),
+        seq($.ident, '=', $.define_path_ident),
         seq($.const_ident, '=', $.path_const_ident),
-        seq($.at_ident, '=', $.path_at_ident),
+        seq($.at_ident, '=', $.define_path_at_ident),
       ),
       optional($.generic_arguments),
     ),
